b419f574f4e243a2b7e4bc35084751a1c757ffc2
miltonzini <miltonzini@gmail.com>
2024-05-07 17:20:10 -0300

    Create register view and route

---
+++ resources/views/register.blade.php
@@ -0,0 +1,10 @@
+@section('title', 'Register')
+<x-layout>
+<main>
+    <section>
+        <div class="container">
+            <h1>TEMP Register</h1>
+        </div>
+    </section>
+</main>
+</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {return view('home');})->name('home');
 Route::get('/login', function () {return view('login');})->name('login');
+Route::get('/register', function () {return view('register');})->name('register');
 
