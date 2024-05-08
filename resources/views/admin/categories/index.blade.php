e248e1582591f396c60001e259f18b921da8fc07
miltonzini <miltonzini@gmail.com>
2024-05-08 13:34:05 -0300

    modularize admin aside and nav

--- resources/views/admin/categories/index.blade.php
+++ resources/views/admin/categories/index.blade.php
@@ -4,87 +4,13 @@
 
     <div id="wrapper">
 
-        <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
-
-            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
-                <div class="sidebar-brand-icon rotate-n-15">
-                    <i class="fas fa-laugh-wink"></i>
-                </div>
-                <div class="sidebar-brand-text mx-3">Lucas Ferro</div>
-            </a>
-
-            <li class="nav-item">
-                <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#products-menu"
-                    aria-expanded="true" aria-controls="products-menu">
-                    <i class="fas fa-fw fa-cog"></i>
-                    <span>Productos</span>
-                </a>
-                <div id="products-menu" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
-                    <div class="bg-white py-2 collapse-inner rounded">
-                        <a class="collapse-item" href="listado-productos.html">Todos los productos</a>
-                        <a class="collapse-item" href="registro-producto.html">Nuevo producto</a>
-                    </div>
-                </div>
-            </li>
-
-            <li class="nav-item">
-                <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#categories-menu"
-                    aria-expanded="true" aria-controls="categories-menu">
-                    <i class="fas fa-fw fa-cog"></i>
-                    <span>Categorías</span>
-                </a>
-                <div id="categories-menu" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
-                    <div class="bg-white py-2 collapse-inner rounded">
-                        <a class="collapse-item" href="listado-categorias.html">Todas las categorías</a>
-                        <a class="collapse-item" href="registro-categoria.html">Nueva categoría</a>
-                    </div>
-                </div>
-            </li>
-
-            <li class="nav-item">
-                <a class="nav-link" href="listado-usuarios.html">
-                    <i class="fas fa-fw fa-user"></i>
-                    <span>Usuarios</span>
-                </a>
-            </li>
-
-            <hr class="sidebar-divider d-none d-md-block">
-
-            <div class="text-center d-none d-md-inline">
-                <button class="rounded-circle border-0" id="sidebarToggle"></button>
-            </div>
-
-        </ul>
+        @include('partials.admin.aside')
 
         <div id="content-wrapper" class="d-flex flex-column">
+        
             <div id="content">
-                <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
-                    <form class="form-inline">
-                        <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
-                            <i class="fa fa-bars"></i>
-                        </button>
-                    </form>
 
-                    <ul class="navbar-nav ml-auto">
-                        <li class="nav-item dropdown no-arrow">
-                            <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
-                                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                <span class="mr-2 d-none d-lg-inline text-gray-600 small">Lucas Ferro</span>
-                                <img class="img-profile rounded-circle"
-                                    src="img/undraw_profile.svg">
-                            </a>
-
-                            <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
-                                <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
-                                    <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
-                                    Cerrar sesión
-                                </a>
-                            </div>
-                        </li>
-
-                    </ul>
-
-                </nav>
+                @include('partials.admin.nav')
 
                 <div class="container-fluid">
                     <div class="row">
---
+++ resources/views/partials/admin/aside.blade.php
@@ -0,0 +1,53 @@
+<!-- Sidebar -->
+    <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+
+        <!-- Sidebar - Brand -->
+        <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+            <div class="sidebar-brand-icon rotate-n-15">
+                <i class="fas fa-laugh-wink"></i>
+            </div>
+            <div class="sidebar-brand-text mx-3">Lucas Ferro</div>
+        </a>
+
+        <li class="nav-item">
+            <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#products-menu"
+                aria-expanded="true" aria-controls="products-menu">
+                <i class="fas fa-fw fa-cog"></i>
+                <span>Productos</span>
+            </a>
+            <div id="products-menu" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+                <div class="bg-white py-2 collapse-inner rounded">
+                    <a class="collapse-item" href="listado-productos.html">Todos los productos</a>
+                    <a class="collapse-item" href="registro-producto.html">Nuevo producto</a>
+                </div>
+            </div>
+        </li>
+
+        <li class="nav-item">
+            <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#categories-menu"
+                aria-expanded="true" aria-controls="categories-menu">
+                <i class="fas fa-fw fa-cog"></i>
+                <span>Categorías</span>
+            </a>
+            <div id="categories-menu" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+                <div class="bg-white py-2 collapse-inner rounded">
+                    <a class="collapse-item" href="listado-categorias.html">Todas las categorías</a>
+                    <a class="collapse-item" href="registro-categoria.html">Nueva categoría</a>
+                </div>
+            </div>
+        </li>
+
+        <li class="nav-item">
+            <a class="nav-link" href="listado-usuarios.html">
+                <i class="fas fa-fw fa-user"></i>
+                <span>Usuarios</span>
+            </a>
+        </li>
+
+        <hr class="sidebar-divider d-none d-md-block">
+
+        <div class="text-center d-none d-md-inline">
+            <button class="rounded-circle border-0" id="sidebarToggle"></button>
+        </div>
+
+    </ul>
