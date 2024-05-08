849d7f6fb1247fe8c4c24b29ce915be9e9deeff3
miltonzini <miltonzini@gmail.com>
2024-05-08 10:50:07 -0300

    Add admin views static content and scripts

--- resources/views/admin/products/create.blade.php
+++ resources/views/admin/products/create.blade.php
@@ -1,10 +1,192 @@
 @section('title', 'Admin / Products / Create')
 <x-adminLayout>
-<main>
-    <section>
-        <div class="container">
-            <h1>Temp Admin / Products / Create</h1>
+<body id="page-top">
+
+    <!-- Page Wrapper -->
+    <div id="wrapper">
+
+        <!-- Sidebar -->
+        <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+
+            <!-- Sidebar - Brand -->
+            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+                <div class="sidebar-brand-icon rotate-n-15">
+                    <i class="fas fa-laugh-wink"></i>
+                </div>
+                <div class="sidebar-brand-text mx-3">Lucas Ferro</div>
+            </a>
+
+            <li class="nav-item">
+                <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#products-menu"
+                    aria-expanded="true" aria-controls="products-menu">
+                    <i class="fas fa-fw fa-cog"></i>
+                    <span>Productos</span>
+                </a>
+                <div id="products-menu" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+                    <div class="bg-white py-2 collapse-inner rounded">
+                        <a class="collapse-item" href="listado-productos.html">Todos los productos</a>
+                        <a class="collapse-item" href="registro-producto.html">Nuevo producto</a>
+                    </div>
+                </div>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#categories-menu"
+                    aria-expanded="true" aria-controls="categories-menu">
+                    <i class="fas fa-fw fa-cog"></i>
+                    <span>Categorías</span>
+                </a>
+                <div id="categories-menu" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+                    <div class="bg-white py-2 collapse-inner rounded">
+                        <a class="collapse-item" href="listado-categorias.html">Todas las categorías</a>
+                        <a class="collapse-item" href="registro-categoria.html">Nueva categoría</a>
+                    </div>
+                </div>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link" href="listado-usuarios.html">
+                    <i class="fas fa-fw fa-user"></i>
+                    <span>Usuarios</span>
+                </a>
+            </li>
+
+            <hr class="sidebar-divider d-none d-md-block">
+
+            <div class="text-center d-none d-md-inline">
+                <button class="rounded-circle border-0" id="sidebarToggle"></button>
+            </div>
+
+        </ul>
+
+        <div id="content-wrapper" class="d-flex flex-column">
+
+            <div id="content">
+
+                <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
+
+                    <form class="form-inline">
+                        <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
+                            <i class="fa fa-bars"></i>
+                        </button>
+                    </form>
+
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item dropdown no-arrow">
+                            <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
+                                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                <span class="mr-2 d-none d-lg-inline text-gray-600 small">Lucas Ferro</span>
+                                <img class="img-profile rounded-circle"
+                                    src="img/undraw_profile.svg">
+                            </a>
+                            <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in"
+                                aria-labelledby="userDropdown">
+                                <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
+                                    <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
+                                    Cerrar sesión
+                                </a>
+                            </div>
+                        </li>
+
+                    </ul>
+
+                </nav>
+
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-lg-9">
+                            <h1 class="h3 mb-2 text-gray-800">Registrar nuevo producto</h1>
+                        </div>
+
+                        <div class="col-lg-3">
+                            <a href="listado-productos.html" class="btn btn-primary float-right">Volver</a>
+                        </div>
+                    </div>
+
+                    <div class="card shadow mb-4">
+                        <div class="card-body">
+                            <form>
+                                <div class="row">
+                                    <div class="col-md-6">
+                                        <label class="control-label">Título *</label>
+                                        <input type="text" name="title" placeholder="Título *" class="form-control">
+                                    </div>
+
+                                    <div class="col-md-6">
+                                        <label class="control-label">Categoría *</label>
+                                        <select name="category" class="form-control">
+                                            <option value="">Seleccionar...</option>
+                                        </select>
+                                    </div>
+                                </div>
+
+                                <div class="row mt-3">
+                                    <div class="col-md-12">
+                                        <label class="control-label">Descripción *</label>
+                                        <textarea class="form-control" name="description"></textarea>
+                                    </div>
+                                </div>
+
+                                <div class="row mt-3">
+                                    <div class="col-md-6">
+                                        <label class="control-label">Precio *</label>
+                                        <input type="text" name="price" placeholder="Precio *" class="form-control">
+                                    </div>
+
+                                    <div class="col-md-6">
+                                        <label class="control-label">Descuento</label>
+                                        <input type="text" name="discount" placeholder="Descuento" class="form-control">
+                                        <small>Dejar "0" o vacío si no aplica</small>
+                                    </div>
+                                </div>
+
+                                <div class="row mt-3">
+                                    <div class="col-md-4 float-end">
+                                        <button type="submit" class="btn btn-info btn-sm">Registrar producto</button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
+
         </div>
-    </section>
-</main>
+
+    </div>
+
+    <!-- Logout Modal-->
+    <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+        aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">¿Seguro que quiere salir del sistema?</h5>
+                    <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">×</span>
+                    </button>
+                </div>
+                <div class="modal-body">Seleccionar "Salir" para cerrar la sesión de su cuenta.</div>
+                <div class="modal-footer">
+                    <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancelar</button>
+                    <a class="btn btn-primary" href="login.html">Salir</a>
+                </div>
+            </div>
+        </div>
+    </div>
+@push('scripts')
+    <!-- Page level plugins -->
+    <script src="vendor/datatables/jquery.dataTables.min.js"></script>
+    <script src="vendor/datatables/dataTables.bootstrap4.min.js"></script>
+
+    <!-- Page level custom scripts -->
+    <script src="js/demo/datatables-demo.js"></script>
+@if (isset($scripts) && !empty($scripts))
+@foreach ($scripts as $script)
+    <script src="{{ asset('public/js/functions/' . $script) }}"></script>
+@endforeach
+@endif
+@endpush
 </x-adminLayout>
--- resources/views/components/adminLayout.blade.php
+++ resources/views/components/adminLayout.blade.php
@@ -10,6 +10,7 @@
     <link rel="stylesheet" href="{{ mix('vendor/sb-admin-2/vendor/fontawesome-free/css/all.min.css') }}">
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
     <link rel="stylesheet" href="{{ mix('vendor/sb-admin-2/css/sb-admin-2.min.css') }}">
+    <link rel="stylesheet" href="{{ mix('vendor/sb-admin-2/vendor/datatables/dataTables.bootstrap4.min.css') }}">
     <title>RedNodo Laravel 01 | @yield('title')</title>
 
 </head>
