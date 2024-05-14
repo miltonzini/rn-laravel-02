4a2a2e12851732df59fa71b9256447700ab47e56
miltonzini <miltonzini@gmail.com>
2024-05-14 16:53:51 -0300

    Minor update

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -8,7 +8,7 @@ use Illuminate\Http\Request;
 class CategoryController extends Controller
 {
     public function index() {
-        $categories = Category::select('id', 'title')->orderBy('id', 'desc')->paginate(1);
+        $categories = Category::select('id', 'title')->orderBy('id', 'desc')->paginate(20);
         $categoriesCount = Category::count();
         $scripts = ['categories.js'];
         return view('admin.categories.index', compact('categories', 'categoriesCount', 'scripts'));
