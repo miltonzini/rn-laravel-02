a9d3a149502474d82a9be8231830ff7aa8d6bb4e
miltonzini <miltonzini@gmail.com>
2024-05-17 12:49:13 -0300

    Minor update

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,7 +10,7 @@ use Illuminate\Support\Facades\Session;
 class UserController extends Controller
 {
     public function index() {
-        $users = User::select('id', 'name', 'surname', 'email', 'created_at')->orderBy('id', 'desc')->paginate(1);
+        $users = User::select('id', 'name', 'surname', 'email', 'created_at')->orderBy('id', 'desc')->paginate(10);
         $usersCount = User::count();
         $scripts = ['users.js'];
         return view('admin.users.index', compact('users', 'usersCount', 'scripts'));
